0cc0730717ae91b3eb63399a27983c7585e9d2ae
Ravzing <audriusez@gmail.com>
2025-02-04 19:09:20 +0200

    getting starter

---
+++ rpg.py
@@ -0,0 +1,9 @@
+"""
+    This is the main file for the RPG game.
+    Run this file to play the game.
+    this file will import the other classes and functions that you need to run the game.
+    this game is a text-based RPG game.
+    need to create a character, explore different areas, and fight enemies.
+    character will have their own stats and abilities.
+    also username and password to save the game.
+"""
